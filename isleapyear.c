8727c72ccb3323ac4c1ab181200154e81e3f62d5
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2023-06-26 20:34:17 -0400

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -5,12 +5,15 @@ int main() {
     int year;
     scanf("%d",&year);
     
-    if (year % 400 == 0)
-		printf("%d was a leap year", year);
-	else if (year % 100 == 0)
-		printf("%d was not a leap year", year);
-	else if (year % 4 == 0)
-		printf("%d was a leap year", year);
-	else 
-		return 0;
+    if(year % 4 == 0) {
+        if(year % 100 != 0 || year % 400 == 0) {
+            printf("%d was a leap year\n", year);
+        } else {
+            printf("%d was not a leap year\n", year);
+        }
+    } else {
+        printf("%d was not a leap year\n", year);
+    }
+
+    return 0;
 }
