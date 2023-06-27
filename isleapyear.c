b07e6a1419f045d99e7ceaf510a7a35151af01e3
Trishala-K1 <121046665+Trishala-K1@users.noreply.github.com>
2023-06-26 20:45:06 -0400

    Update isleapyear.c

--- isleapyear.c
+++ isleapyear.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-int main() {
-   int year;
-    printf("Enter a year: ");
-    scanf("%d", &year);
+#include <stdlib.h>
 
-   
-   if (year % 400 == 0) {
-      printf("%d is a leap year.", year);
-   }
-  
-   else if (year % 100 == 0) {
-      printf("%d is not a leap year.", year);
-   }
-
-   else if (year % 4 == 0) {
-      printf("%d is a leap year.", year);
-   }
-   
-   else {
-      printf("%d is not a leap year.", year);
-   }
-
-   return 0;
+int main(int argc, char** argv) {
+    int year;
+    sscanf (argv[1],"%d",&year);
+    if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0)){
+      printf("%d was a leap year", year);
+    }
+    else{
+      printf("%d was not a leap year", year);
+    }
+    return 0;
 }
